ceed0c285966de6126461cede8c5f7cbae24ad45
ajay-2805 <66821419+ajay-2805@users.noreply.github.com>
2021-02-20 22:58:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+	
+    <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed4;
+		
+	}
+	tr{
+		text-align:center;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("bank-with-coins.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+
+	<div id="header">
+       <br>
+	   <br>
+	   <br>
+	   <br>
+	   <br>
+	   <h2 style=" position left; font-family:Agency FB; font-size: 100px;color:#808080;text-shadow: 1px 1px grey;">Welcome</h2>
+       
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+				<button class="button button1" type="button" href="getdetail.php">View Users</button>
+				</a>
+				<br><br>
+				<a href="transaction.php">
+                <button class="button button2" type="button" href="transaction.php">Tansaction History</button>
+				</a>
+				
+               </tr>
+
+            </table>
+    </div>
+</body>
+</html>
